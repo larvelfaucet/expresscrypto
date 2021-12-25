c5c20d19d6b399ad6511cb0c139900e08bc9bc43
vino <vino.gcs@gmail.com>
2021-12-25 11:13:34 +0530

    Fixed ApiResponse

--- src/ApiResponse.php
+++ src/ApiResponse.php
@@ -8,8 +8,8 @@ class ApiResponse {
 
     public function __construct($data){
         $this->data = new \stdClass();
-        $this->status = $data->status;
-        $this->message = $data->message;
+        $this->status = $data['status'];
+        $this->message = $data['message'];
         foreach($data as $key => $value){
             if(!in_array($key, ['status', 'message'])){
                 $this->data->$key = $value;
